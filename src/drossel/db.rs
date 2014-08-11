7c0faa7c7e59c6cdd4333e18709a7b61146e6670
Florian Gilcher <florian.gilcher@asquera.de>
2014-08-11 15:26:27 +0200

    Event implementation for PING

--- src/drossel/db.rs
+++ src/drossel/db.rs
@@ -1,5 +1,11 @@
-use super::store::{Queue};
+use super::store::*;
 use std::collections::dlist::DList;
+use super::super::*;
+use strand::mutable::Strand;
+use strand::mutable::Event;
+use strand::strand::Mutable;
+use strand::strand;
+use strand::errors::{Errors};
 
 pub struct DB {
   queue: Queue
@@ -8,5 +14,24 @@ pub struct DB {
 impl DB {
   pub fn new() -> DB {
     DB { queue: Queue { state: DList::new() } }
+  }
+
+  pub fn execute<S, T, V: Event<BinaryList,S>+Command>(mut self, effect: V) -> Result<S, Errors> {
+    self.queue.evolve(&effect)
+  }
+}
+
+
+impl Event<BinaryList,&'static str> for Ping {
+  fn precondition(&self, _: &BinaryList) -> Result<(), Errors> {
+    Ok(())
+  }
+
+  fn action(&self, state: &mut BinaryList) -> Result<&'static str, Errors> {
+    Ok("PONG")
+  }
+
+  fn postcondition(&self, _: &BinaryList) -> Result<(), Errors> {
+    Ok(())
   }
 }
