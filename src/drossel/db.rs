063da4ef5ae144f9bb345cab5262db35eeddac85
Florian Gilcher <florian.gilcher@asquera.de>
2014-08-17 21:57:38 +0200

    Clean up DB tests

--- src/drossel/db.rs
+++ src/drossel/db.rs
@@ -94,47 +94,37 @@ impl AsSendableEvent<BinaryList, DBResult> for Command {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use drossel::store::*;
-  use std::collections::dlist::DList;
-  use drossel::events::{AsEvent};
+  use strand::mutable::{AsSendableEvent};
   use commands::util::get_command;
-  use strand::mutable::Strand;
-  use strand::mutable::Event;
-  use strand::strand::Mutable;
-  use strand::strand;
-  use strand::errors::{Errors};
 
   #[test]
   fn test_db_ping() {
     let command = get_command("PING".as_bytes().to_vec()).unwrap();
-    let mut db = box DB::new();
-    let res = (*command).as_event(|event| {
-      db.execute(event)
-    });
+    let mut db = DB::new();
+    let event = (*command).as_sendable_event();
+    let res = db.execute(event);
     assert_eq!(Pong, res.unwrap())
   }
 
   #[test]
   fn test_db_set() {
     let command = get_command("SET test_queue 0 test_string".as_bytes().to_vec()).unwrap();
-    let mut db = box DB::new();
-    let res = (*command).as_event(|event| {
-      db.execute(event)
-    });
+    let mut db = DB::new();
+    let event = (*command).as_sendable_event();
+    let res = db.execute(event);
+    assert!(res.is_ok());
     assert_eq!(1, db.items())
   }
 
   #[test]
   fn test_db_get() {
     let set = get_command("SET test_queue 0 test_string".as_bytes().to_vec()).unwrap();
-    let mut db = box DB::new();
-    let res = (*set).as_event(|event| {
-      db.execute(event)
-    });
+    let mut db = DB::new();
+    let event1 = (*set).as_sendable_event();
+    assert!(db.execute(event1).is_ok());
     let get = get_command("GET test_queue".as_bytes().to_vec()).unwrap();
-    let res = (*get).as_event(|event| {
-      db.execute(event)
-    });
+    let event2 = (*get).as_sendable_event();
+    assert!(db.execute(event2).is_ok());
     assert_eq!(0, db.items())
   }
 }
