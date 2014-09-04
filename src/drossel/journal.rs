f98d40615dda32e90a2c214eb2e366054dd35b1d
Florian Gilcher <florian.gilcher@asquera.de>
2014-09-04 00:42:37 -0700

    Simple journal implementation with push and pop

--- src/drossel/journal.rs
+++ src/drossel/journal.rs
@@ -1,4 +1,9 @@
-#[deriving(Show,PartialEq)]
+use leveldb::database::Database;
+use leveldb::database::binary::Interface;
+use leveldb::options::{Options,WriteOptions,ReadOptions};
+
+#[deriving(Show,PartialEq,Eq,PartialOrd,Ord)]
+#[repr(u64)]
 pub enum KeyType {
   Queue,
   Chunk
@@ -35,11 +40,72 @@ impl Key {
 
     unsafe { f(transmute::<_, [u8, ..16]>(self)) }
   }
+
+  pub fn compare(&self, other: &Key) -> i32 {
+    if self.keytype < other.keytype {
+      return -1
+    }
+    if self.keytype > other.keytype {
+      return 1
+    }
+    if self.id < other.id {
+      return -1
+    }
+    if self.id > other.id {
+      return 1
+    }
+    0
+  }
+}
+
+struct Journal {
+  db: Database,
+  queue_head: Key,
+  queue_tail: Key,
+}
+
+impl Journal {
+  pub fn new(path: Path) -> Journal {
+    let mut options = Options::new();
+    options.create_if_missing(true);
+    let db = Database::open(path, options).unwrap();
+    let queue_head = Key { keytype: Queue, id: 0 };
+    let queue_tail = Key { keytype: Queue, id: 0 };
+    Journal { db: db, queue_head: queue_head, queue_tail: queue_tail }
+  }
+
+  pub fn push(&mut self, data: &[u8]) {
+    self.queue_head.as_slice(|key| {
+      let mut write_options = WriteOptions::new();
+      write_options.sync(true);
+      self.db.put(write_options, key, data).unwrap_or_else(|err| {
+        fail!("error writing to journal: {}", err)
+      })
+    });
+
+    self.queue_head.id = self.queue_head.id + 1;
+  }
+
+  pub fn pop(&mut self) -> Option<Vec<u8>> {
+    if self.queue_head.id > self.queue_tail.id {
+      let res = self.queue_tail.as_slice(|key| {
+        let read_options = ReadOptions::new();
+        self.db.get(read_options, key).unwrap_or_else(|err| {
+          fail!("error reading from journal: {}", err)
+        })
+      });
+      self.queue_tail.id = self.queue_tail.id + 1;
+      return res;
+    } else {
+      None
+    }
+  }
 }
 
 #[cfg(test)]
 mod tests {
-  use drossel::journal::{Key,Queue};
+  use drossel::journal::{Key,Queue,Chunk,Journal};
+  use std::io::TempDir;
 
   #[test]
   fn test_roundtrip() {
@@ -49,5 +115,35 @@ mod tests {
       assert_eq!(roundtrip.id, 123);
       assert_eq!(roundtrip.keytype, Queue);
     });
+  }
+
+  #[test]
+  fn test_compare() {
+    let key = Key { keytype: Queue, id: 123 };
+    let key2 = Key { keytype: Chunk, id: 123 };
+    let key3 = Key { keytype: Queue, id: 124 };
+    assert_eq!(-1, key.compare(&key2));
+    assert_eq!(1, key2.compare(&key));
+    assert_eq!(-1, key.compare(&key3));
+    assert_eq!(1, key3.compare(&key));
+    assert_eq!(0, key.compare(&key));
+  }
+
+  #[test]
+  fn test_journal() {
+    let dir = TempDir::new("journal_test").unwrap();
+    let mut journal = Journal::new(dir.path().join("journal_test"));
+    let res = journal.pop();
+    assert!(res.is_none());
+    journal.push(&[1]);
+    journal.push(&[2]);
+    let res2 = journal.pop();
+    assert!(res2.is_some());
+    assert_eq!(Some(vec![1 as u8]), res2);
+    let res3 = journal.pop();
+    assert!(res3.is_some());
+    assert_eq!(Some(vec![2 as u8]), res3);
+    let res4 = journal.pop();
+    assert!(res4.is_none());
   }
 }
